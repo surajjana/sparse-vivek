352aa923da0457df30b4814153373e172af1dcce
Suraj Kumar Jana <surajjana2@gmail.com>
2016-05-12 12:52:34 +0530

    new commit

--- q_all.php
+++ q_all.php
@@ -16,6 +16,8 @@ $name = $_POST['name'];
 $gender = $_POST['gender'];
 $age_range = $_POST['age'];
 
+$sql = '';
+
 if((strlen($name) != 0) && (strcmp($gender,'0') != 0) && (strcmo($age_range,'0'))){
     $sql = "select * from info where name='".$name."' and gender='".$gender."' and age_range='".$age_range."'"
 }else{
